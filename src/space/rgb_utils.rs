5d25911334b03e2202986cb8ace9b57423629457
Hector <hector.business@proton.me>
2024-11-14 13:06:55 -0300

    Rename variables for clarity

--- src/space/rgb_utils.rs
+++ src/space/rgb_utils.rs
@@ -62,49 +62,49 @@ pub fn planes_of_axis(incr: usize, size: usize, axis: Axis) -> Vec<Vec<Vec<Color
 
 fn plane_colorize(pca: &PlaneColorizeArgs, axis: &Axis) -> Vec<Vec<Color>> {
     let mut plane: Vec<Vec<Color>> = vec![vec![]; pca.size];
-    let mut vw = PlaneValues::new();
+    let mut plane_vals = PlaneValues::new();
 
-    for i in 0..pca.size {
-        vw.outer_idx = i;
-        if i == 0 {
-            vw.outer_val += pca.incr - 1;
+    for line in 0..pca.size {
+        plane_vals.outer_idx = line;
+        if line == 0 {
+            plane_vals.outer_val += pca.incr - 1;
         } else {
-            vw.outer_val += pca.incr;
+            plane_vals.outer_val += pca.incr;
         }
 
-        for j in 0..pca.size {
-            vw.inner_idx = j;
-            if j == 0 {
-                vw.inner_val += pca.incr - 1;
+        for columns in 0..pca.size {
+            plane_vals.inner_idx = columns;
+            if columns == 0 {
+                plane_vals.inner_val += pca.incr - 1;
             } else {
-                vw.inner_val += pca.incr;
+                plane_vals.inner_val += pca.incr;
             }
 
             match axis {
                 Axis::Red => {
-                    red_axis(&mut plane, &vw, pca.axis_val);
+                    red_axis(&mut plane, &plane_vals, pca.axis_val);
                 }
 
                 Axis::Green => {
-                    green_axis(&mut plane, &vw, pca.axis_val);
+                    green_axis(&mut plane, &plane_vals, pca.axis_val);
                 }
 
                 Axis::Blue => {
-                    blue_axis(&mut plane, &vw, pca.axis_val);
+                    blue_axis(&mut plane, &plane_vals, pca.axis_val);
                 }
             }
         }
-        vw.inner_val = 0
+        plane_vals.inner_val = 0
     }
     plane
 }
 
-fn red_axis(plane: &mut Vec<Vec<Color>>, vw: &PlaneValues, axis_val: usize) {
-    plane[vw.outer_idx].push(Color::new(axis_val, vw.outer_val, vw.inner_val));
+fn red_axis(plane: &mut Vec<Vec<Color>>, pv: &PlaneValues, axis_val: usize) {
+    plane[pv.outer_idx].push(Color::new(axis_val, pv.outer_val, pv.inner_val));
 }
-fn green_axis(plane: &mut Vec<Vec<Color>>, vw: &PlaneValues, axis_val: usize) {
-    plane[vw.outer_idx].push(Color::new(vw.outer_val, axis_val, vw.inner_val));
+fn green_axis(plane: &mut Vec<Vec<Color>>, pv: &PlaneValues, axis_val: usize) {
+    plane[pv.outer_idx].push(Color::new(pv.outer_val, axis_val, pv.inner_val));
 }
-fn blue_axis(plane: &mut Vec<Vec<Color>>, vw: &PlaneValues, axis_val: usize) {
-    plane[vw.outer_idx].push(Color::new(vw.outer_val, vw.inner_val, axis_val));
+fn blue_axis(plane: &mut Vec<Vec<Color>>, pv: &PlaneValues, axis_val: usize) {
+    plane[pv.outer_idx].push(Color::new(pv.outer_val, pv.inner_val, axis_val));
 }
